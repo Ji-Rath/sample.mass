4c02165ea04e9992aa948e71f0cc0a1d5e6d0ed3
Arkena <ads.neuroshok@gmail.com>
2024-07-15 12:12:47 +0200

    [update] 5.4

--- Source/MassSampleEditor.Target.cs
+++ Source/MassSampleEditor.Target.cs
@@ -8,7 +8,7 @@ public class MassSampleEditorTarget : TargetRules
     public MassSampleEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V2;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
 
         ExtraModuleNames.AddRange( new string[] { "MassSample" } );
     }
--- Source/MassSampleServer.Target.cs
+++ Source/MassSampleServer.Target.cs
@@ -8,7 +8,7 @@ public class MassSampleServerTarget : TargetRules //Change this line according t
 	public MassSampleServerTarget(TargetInfo Target) : base(Target) //Change this line according to the name of your project
 	{
 		Type = TargetType.Server;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		ExtraModuleNames.Add("MassSample"); //Change this line according to the name of your project
 	}
 }
